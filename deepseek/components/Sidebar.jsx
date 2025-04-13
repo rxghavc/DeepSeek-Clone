84689fb89326264f1fe383e76749b934620477d3
rxghavc <r.commandur@gmail.com>
2025-04-13 20:08:46 +0100

    added profile icon display for clerk authentication

--- deepseek/components/Sidebar.jsx
+++ deepseek/components/Sidebar.jsx
@@ -2,9 +2,11 @@ import Image from 'next/image'
 import { assets } from "../assets/assets";
 import React from 'react'
 import { useClerk, UserButton } from '@clerk/nextjs'
+import { useAppContext } from '@/context/AppContent';
 
 const Sidebar = ({expand, setExpand}) => {
     const {openSignIn} = useClerk()
+    const {user} = useAppContext()
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
         <div>
@@ -49,8 +51,10 @@ const Sidebar = ({expand, setExpand}) => {
                 </div>
                 {expand && <><span>Get App</span> <Image alt='' src={assets.new_icon} /></>}
             </div>
-            <div onClick={openSignIn} className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
-                <Image src={assets.profile_icon} alt='' className='w-7' />
+            <div onClick={user ? null : openSignIn} className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+                {
+                    user ? <UserButton/> : <Image src={assets.profile_icon} alt='' className='w-7' />
+                }
                 {expand && <span>My Profile</span>}
             </div>
         </div>
