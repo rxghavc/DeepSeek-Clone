094ff07d816c834e5538c5486ef7d990cf4c34da
rxghavc <r.commandur@gmail.com>
2025-04-13 19:55:50 +0100

    updated sidebar for clerk profiling

--- deepseek/components/Sidebar.jsx
+++ deepseek/components/Sidebar.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
 import { assets } from "../assets/assets";
 import React from 'react'
+import { useClerk, UserButton } from '@clerk/nextjs'
 
 const Sidebar = ({expand, setExpand}) => {
+    const {openSignIn} = useClerk()
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
         <div>
@@ -47,7 +49,7 @@ const Sidebar = ({expand, setExpand}) => {
                 </div>
                 {expand && <><span>Get App</span> <Image alt='' src={assets.new_icon} /></>}
             </div>
-            <div className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+            <div onClick={openSignIn} className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
                 <Image src={assets.profile_icon} alt='' className='w-7' />
                 {expand && <span>My Profile</span>}
             </div>
