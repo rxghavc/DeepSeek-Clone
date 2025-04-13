f336ae2083712e0c0b82ec3f29444a8429327b57
rxghavc <r.commandur@gmail.com>
2025-04-13 20:15:45 +0100

    added user profile for sidebar

--- deepseek/components/Sidebar.jsx
+++ deepseek/components/Sidebar.jsx
@@ -5,7 +5,7 @@ import { useClerk, UserButton } from '@clerk/nextjs'
 import { useAppContext } from '@/context/AppContent';
 
 const Sidebar = ({expand, setExpand}) => {
-    const {openSignIn} = useClerk()
+    const {openSignIn, openUserProfile} = useClerk()
     const {user} = useAppContext()
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
