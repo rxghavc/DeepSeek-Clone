dd18da9786fe93e7acee168f11a9c9a5edbcebed
rxghavc <r.commandur@gmail.com>
2025-04-13 15:00:40 +0100

    updated #3

--- deepseek/app/page.js
+++ deepseek/app/page.js
@@ -1,7 +1,22 @@
 import Image from "next/image";
+import { assets } from "../assets/assets";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [expand, setExpand] = useState(false);
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <div></div>
+    <div>
+      <div className="flex h-screen">
+        {/* -- Sidebar --*/}
+        <div>
+          <Image className="rotate-180" src={assets.menu_icon} alt="" width={30} height={30} />
+          <Image className="opacity-70" src={assets.chat_icon} alt="" width={30} height={30} />
+        </div>
+      </div>
+    </div>
   );
 }
