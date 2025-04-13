c36fe32fcc00a17b7fd4622ccdc307ce1338f8c3
rxghavc <r.commandur@gmail.com>
2025-04-13 15:41:31 +0100

    added sidebar hook

--- deepseek/app/page.jsx
+++ deepseek/app/page.jsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { assets } from "../assets/assets";
 import { useState } from "react";
+import Sidebar from "@/components/Sidebar";
 
 export default function Home() {
 
@@ -13,7 +14,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex h-screen">
-        {/* -- Sidebar --*/}
+        <Sidebar expand={expand} setExpand={setExpand}/>
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image onClick={()=> (expand ? setExpand(false) : setExpand(true))} className="rotate-180" src={assets.menu_icon} alt=""/>
