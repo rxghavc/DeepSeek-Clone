6ec655635ef0f79e09a843b4fbf6e052bb44df54
rxghavc <r.commandur@gmail.com>
2025-04-13 20:08:31 +0100

    imported react hook

--- deepseek/context/AppContent.jsx
+++ deepseek/context/AppContent.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 import { useUser } from "@clerk/nextjs"
 
 export const AppContext = createContext();
