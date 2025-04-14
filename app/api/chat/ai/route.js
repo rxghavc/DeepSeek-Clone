34d02f9ded9aa42280922eb8dd286113cb113755
rxghavc <r.commandur@gmail.com>
2025-04-14 12:39:00 +0100

    added route for DeepSeek AI

---
+++ app/api/chat/ai/route.js
@@ -0,0 +1,18 @@
+import OpenAI from "openai";
+
+// Initialize OpenAI with DeepSeek API key
+const openai = new OpenAI({
+        baseURL: 'https://api.deepseek.com',
+        apiKey: '<DeepSeek API Key>'
+});
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: "system", content: "You are a helpful assistant." }],
+    model: "deepseek-chat",
+  });
+
+  console.log(completion.choices[0].message.content);
+}
+
+main();
